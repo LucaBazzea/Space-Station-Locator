bcee2d00f42af7cf71a565952468dadc954164ce
LucaBazzea <luca.bazzea@gmail.com>
2022-06-13 17:27:41 +0200

    Adjust map size and zoom

--- core/views.py
+++ core/views.py
@@ -44,7 +44,7 @@ print(f"\nCoordinates: {latitude}, {longitude}")
 
 def get_index(request):
 
-    map = folium.Map(location=[latitude, longitude], zoom_start=5, width=800, height=400)
+    map = folium.Map(location=[latitude, longitude], zoom_start=4, width=800, height=500)
     map = map._repr_html_() # Convert python notebook map into HTML
 
     context = {
